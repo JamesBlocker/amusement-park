2152b4d2db383bf0ba3ce578f2423297651a3c57
Byron Blocker <byronblocker@gmail.com>
2017-09-14 14:09:37 -0700

    add js to show rides based on height

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
     var userHeight = parseInt(prompt("What is your height in inches? \n *be honest"));
 
-    $('#opener').fadeOut(1500);
-    
+    $('#opener').fadeOut(1000);
+    $('ul').fadeIn(1500);
+
+    if (userHeight <= 64) {
+        $('#shortPanel').fadeIn(3000);
+        $('.short').css("text-decoration", "underline");
+    } else if (userHeight >= 77) {
+        $('#tallPanel').fadeIn(3000);
+    } else {
+        $('#normalPanel').fadeIn(3000);
+    }
 
 
 });
